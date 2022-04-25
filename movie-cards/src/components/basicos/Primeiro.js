23084de0023e2bd2dbb26ffc7477a4a0a4675a2a
BushidoNJ <alllanazevedo8@gmail.com>
2022-04-25 13:29:11 -0300

    test

---
+++ movie-cards/src/components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Primeiro() {
+    const msg = 'Select'
+    return (
+        <div>
+            <h2>Movie </h2>
+            <p> { msg } </p>
+        </div>
+    )
+}
